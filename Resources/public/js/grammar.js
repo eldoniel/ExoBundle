72ceb283690292e525481b88ec47214291d4b787
Eldoniel <nicofourdu@hotmail.fr>
2015-07-16 10:47:35 +0200

    Set new js into a dedicaded file

---
+++ Resources/public/js/grammar.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    var collectionHolder = $('ul.contents');
+    collectionHolder.find('li').each(function () {
+        addTagFormDeleteLink($(this));
+    });
+
+    $("#add-content").on('click', function (e) {
+        e.preventDefault();
+        addTagForm(collectionHolder);
+    });
+});
+
+$(document).ready(function () {
+    var collectionHolder = $('ul.instructions');
+    collectionHolder.find('li').each(function () {
+        addTagFormDeleteLink($(this));
+    });
+
+    $("#add-instruction").on('click', function (e) {
+        e.preventDefault();
+        addTagForm(collectionHolder);
+    });
+});
+
+$(document).ready(function () {
+    var collectionHolder = $('ul.complementaryInformations');
+    collectionHolder.find('li').each(function () {
+        addTagFormDeleteLink($(this));
+    });
+
+    $("#add-complementaryInformation").on('click', function (e) {
+        e.preventDefault();
+        addTagForm(collectionHolder);
+    });
+});
+
+function addTagForm(collectionHolder) {
+    // Récupère l'élément ayant l'attribut data-prototype comme expliqué plus tôt
+    var prototype = collectionHolder.attr('data-prototype');
+    console.log(prototype);
+    // Remplace '__name__' dans le HTML du prototype par un nombre basé sur
+    // la longueur de la collection courante
+    var newForm = prototype.replace(/__name__/g, collectionHolder.children().length);
+    var newFormLi = $('<li class="list-group-item col-md-12"></div>').append(newForm);
+    collectionHolder.append(newFormLi);
+    addTagFormDeleteLink(newFormLi);
+}
+function addTagFormDeleteLink(tagFormLi) {
+    var $removeFormA = $('<a href="#">Supprimer</a>');
+    tagFormLi.append($removeFormA);
+    $removeFormA.on('click', function (e) {
+        // empêche le lien de créer un « # » dans l'URL
+        e.preventDefault();
+        // supprime l'élément li pour le formulaire de tag
+        tagFormLi.remove();
+    });
+}
